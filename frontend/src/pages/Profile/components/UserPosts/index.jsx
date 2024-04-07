d14fef443f0f84f8ffe4a477d0e67ca690383590
atian155 <atianthony@outlook.com>
2024-04-07 23:20:00 +0300

    Added styling to posts

--- frontend/src/pages/Profile/components/UserPosts/index.jsx
+++ frontend/src/pages/Profile/components/UserPosts/index.jsx
@@ -5,8 +5,9 @@ import { useRequest } from "../../../../core/hooks/useRequest";
 import "./styles.css";
 // Icons
 import { IoMdGrid } from "react-icons/io";
-
 import { TbUserSquare } from "react-icons/tb";
+import { FaHeart, FaComments } from "react-icons/fa6";
+import { BsImages } from "react-icons/bs";
 
 const UserPosts = ({ posts }) => {
   return (
@@ -25,7 +26,16 @@ const UserPosts = ({ posts }) => {
           const { id, caption, created_at, images } = post;
           return (
             <div className="post" key={id}>
+              {images.length > 1 && <BsImages size={24} className="multiple-images" />}
               <img src={images[0].image_url} />
+              <div className="likes-and-comments">
+                <div>
+                  <FaHeart /> 288
+                </div>
+                <div>
+                  <FaComments /> 11
+                </div>
+              </div>
             </div>
           );
         })}
